c15529b3752e57e9f2e9cdca65babb7c163a2f8c
KAUAN LOPES PEREIRA <25132925@CFP127ALUNO.LOCAL>
2025-09-15 08:39:40 -0300

    Criação do arquivo app.js e funcoes .js, adicionado funcoes necessarias para a base da api

---
+++ modulo/funcoes.js
@@ -0,0 +1,50 @@
+'use strict'
+
+/********************************************************************************************
+* Objetivo: Arquivo de funções para gerenciar a API de estados e cidades.
+* Autor: Kauan Lopes Pereira
+* Data: 15/09/2025
+* Versão: 1.0.0.0
+********************************************************************************************/
+
+/* Comentário em bloco */
+// Comentário em linha
+
+/********************************************************************************************
+********************************* COMANDOS UTILIZADOS ***************************************
+
+******************************** BIBLIOTECAS UTILIZADAS *************************************
+
+********************************************************************************************/
+
+
+
+// Retorna todos os estados
+function getAllEstados(){
+
+}
+
+// Retorna um estado pesquisando pela sigla
+function getEstadoBySigla(sigla){
+
+}
+
+// Retorna a capital referente a um estado pesquisando pela sigla
+function getCapitalBySigla(sigla){
+
+}
+
+// Retorna todos os estados presentes em uma localidade pesquisando pela região
+function getEstadosByRegiao(regiao){
+
+}
+
+// Retorna uma lista de estados referenta as capitais do pais
+function getVerifyCapitaisDoPais(){
+
+}
+
+// Retorna uma lista de cidades pesquisando pela sigla do estado
+function getCidadesBySigla(sigla){
+
+}
